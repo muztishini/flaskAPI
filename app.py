96f591b4e54a305ba38ec26860699a71b5fdfc50
muztishini <a.kroshakov@yandex.ru>
2024-05-07 17:21:10 +0500

    second commit

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db.init_app(app)
 api = Api(app)
 
-api.add_resource(TaskList, '/tasks', endpoint = 'tasks')
-api.add_resource(TaskApi, '/task/<int:task_id>', endpoint = 'task')
+api.add_resource(TaskList, '/tasks', endpoint='tasks')
+api.add_resource(TaskApi, '/task/<int:task_id>', endpoint='task')
 
 if __name__ == "__main__":
     # with app.app_context():
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
