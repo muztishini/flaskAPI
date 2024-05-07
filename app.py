cbaa2eaa2f767b0e756887303e9e208b6a34a2f4
muztishini <a.kroshakov@yandex.ru>
2024-05-07 17:04:02 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from models import db
+from resources import TaskList, TaskApi
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db.init_app(app)
+api = Api(app)
+
+api.add_resource(TaskList, '/tasks', endpoint = 'tasks')
+api.add_resource(TaskApi, '/task/<int:task_id>', endpoint = 'task')
+
+if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
+    app.run(debug=True)
---
+++ resources.py
@@ -0,0 +1,54 @@
+from flask_restful import Resource
+from flask import request
+from models import Task, db
+
+
+class TaskList(Resource):
+    def get(self):
+        tasks = db.session.query(Task).all()
+        task_list = [{'id': task.id, 'description': task.description}
+                     for task in tasks]
+        return {'tasks': task_list}
+
+    def post(self):
+        task_data = request.get_json()
+        if not task_data:
+            return {'message': 'No input data provided'}, 400
+        description = task_data.get('description')
+        if not description:
+            return {'message': 'Description is required'}, 400
+        new_task = Task(description=description)
+        db.session.add(new_task)
+        db.session.commit()
+        return {'message': 'Task added', 'task': {'id': new_task.id, 'description': new_task.description}}
+
+
+class TaskApi(Resource):
+    def get(self, task_id):
+        task = db.session.query(Task).get(task_id)
+        if task:
+            return {'id': task.id, 'description': task.description}
+        else:
+            return {"message": "Task not found"}
+
+    def put(self, task_id):
+        task = db.session.query(Task).get(task_id)
+        if task:
+            task_data = request.get_json()
+            description = task_data.get('description')
+            if not description:
+                return {'message': 'Description is required'}, 400
+            task.description = description
+            db.session.commit()
+            return {'message': 'Task update', 'task': {'id': task.id, 'description': task.description}}
+        else:
+            return {"message": "Task not found"}
+
+    def delete(self, task_id):
+        task = db.session.query(Task).get(task_id)
+        if task:
+            db.session.delete(task)
+            db.session.commit()
+            return {'message': f"Task {task_id} deleted"}
+        else:
+            return {"message": "Task not found"}
